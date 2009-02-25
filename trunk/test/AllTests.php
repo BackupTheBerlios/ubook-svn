cf1b16e251a19536edc983a1da756a0f1909e092
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2009-02-25 15:57:27 +0000

    untested: AllTests tries to read actual tests automatically

--- trunk/test/AllTests.php
+++ trunk/test/AllTests.php
@@ -6,22 +6,38 @@
 
 require_once 'PHPUnit/Framework.php';
 
+/*
 require_once 'ExternalBookListTest.php';
 require_once 'HttpUrlTest.php';
 require_once 'MailerTest.php';
 require_once 'MessageTest.php';
 require_once 'PhpTest.php';
+*/
 
 class AllTests {
 
 	public static function suite() {
 		$suite = new PHPUnit_Framework_TestSuite('uBook Tests');
+		self::addTests($suite);
+/*
 		$suite->addTestSuite('ExternalBookListTest');
 		$suite->addTestSuite('HttpUrlTest');
 		$suite->addTestSuite('MailerTest');
 		$suite->addTestSuite('MessageTest');
 		$suite->addTestSuite('PhpTest');
+*/
 		return $suite;
+	}
+
+	public static function addTests(&$suite) {
+		$handle = opendir('.');
+		while (($file = readdir($handle)) !== false) {
+			if (substr($file, -8) == 'Test.php') {
+				require_once $file;
+				$suite->addTestSuite(substr($file, 0, -4));
+			}
+		}
+		closedir($handle);
 	}
 	
 }
