ff965b8103d3bc72f81c73deab9fa4dfdd134b7e
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2009-02-26 12:52:20 +0000

    automated test-reading

--- trunk/PhpUnitTests.php
+++ trunk/PhpUnitTests.php
@@ -4,18 +4,32 @@
  * Copyright (C) 2009 Maikel Linke
  */
 
-require_once 'PHPUnit/Framework.php';
+/*
+ * Run this file via:
+ * shell> phpunit PhpUnitTests.php
+ */
 
-require_once 'test/AllTests.php';
+require_once 'PHPUnit/Framework.php';
 
 class PhpUnitTests {
 	
 	public static function suite() {
 		$suite = new PHPUnit_Framework_TestSuite('uBook');
-		$suite->addTest(AllTests::suite());
+		self::addTests($suite);
 		return $suite;
 	}
 	
+	public static function addTests(&$suite) {
+		$testDir = 'test/';
+		$handle = opendir($testDir);
+		while (($file = readdir($handle)) !== false) {
+			if (substr($file, -8) == 'Test.php') {
+				require_once $testDir.$file;
+				$suite->addTestSuite(substr($file, 0, -4));
+			}
+		}
+		closedir($handle);
+	}
 	
 }
 ?>
