6c1e03ce2d781737304ee087034f1522ae6971d3
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2009-06-05 20:26:11 +0000

    tools/Image: version uBookBielefeld

--- trunk/tools/Image.php
+++ trunk/tools/Image.php
@@ -5,112 +5,119 @@
  */
 
 /**
- * This class handles an image of a book.
+ * This handles an image of a book.
  */
 class Image {
 
-	private $id;
+    /**
+     * @var String PATH of image folder.
+     */
+    const PATH = 'img/';
 
-	/**
-	 * Constructor saving book id and determining image path
-	 *
-	 * @param int $id
-	 * @return Image
-	 */
-	public function __construct($id) {
-		$this->id = (int) $id;
-	}
+    /**
+     * Tests wether the upload-function is available.
+     * @return boolean, true: uploads are available, false: uploads are not available
+     */
+    public static function uploadable() {
+        if (!is_writable(self::PATH)) return false;
+        if (!defined('GD_VERSION')) return false;
+        return true;
+    }
 
-	/**
-	 * Reads the uploaded file and writes the image as png to the image dir.
-	 */
-	public function moveUploaded() {
-		if (count($_FILES) != 1) return;
-		if (!isset($_FILES['image'])) return;
-		$tmp_name = $_FILES['image']['tmp_name'];
-		if (!is_uploaded_file($tmp_name)) return;
-		$imageSize = getimagesize($tmp_name);
-		switch ($imageSize[2]) {
-			case IMAGETYPE_GIF:
-				$image = imagecreatefromgif($tmp_name);
-				break;
-			case IMAGETYPE_JPEG:
-				$image = imagecreatefromjpeg($tmp_name);
-				break;
-			case IMAGETYPE_PNG:
-				$image = imagecreatefrompng($tmp_name);
-				break;
-			default:
-				return;
-		}
-		$this->delete();
-		imagepng($image, 'img/'.$this->id.'.png');
-		$max_thumb_width = 250;
-		$max_thumb_height = 250;
-		$img_width = imagesx($image);
-		$img_height = imagesy($image);
-		if ($img_width > $max_thumb_width || $img_height > $max_thumb_height) {
-			if ($img_width > $img_height) {
-				$thumb_width = $max_thumb_width;
-				$thumb_height = $thumb_width * $img_height / $img_width;
-			}
-			else {
-				$thumb_height = $max_thumb_height;
-				$thumb_width = $thumb_height * $img_width / $img_height;
-			}
-			$thumb = imagecreatetruecolor($thumb_width, $thumb_height);
-			imagecopyresized($thumb, $image, 0, 0, 0, 0, $thumb_width, $thumb_height, $img_width, $img_height);
-			imagepng($thumb, 'img/'.$this->id.'_thumb.png');
-			imagedestroy($thumb);
-		}
-		imagedestroy($image);
-		return true;
-	}
+    /**
+     * Returns HTML-Code containing the right img-Tag.
+     *
+     * @param int $id The book's id.
+     * @return String HTML-Code or empty (no image present).
+     */
+    public static function imgTag($id) {
+        $imgURL = self::PATH . $id . '.png';
+        if (!is_file($imgURL)) return '';
+        $thumbURL = self::PATH . $id . '_thumb.png';
+        if (is_file($thumbURL)) {
+            $thumbSize = getimagesize($thumbURL);
+            $tag = '<img src="'.$thumbURL.'" '.$thumbSize[3].' class="bookImage" />';
+            $tag = '<a href="'.$imgURL.'" target="_blank" title="Bild in Originalgröße">'.$tag.'</a>';
+        }
+        else {
+            $tag = '<img src="'.$imgURL.'" class="bookImage" />';
+        }
+        return $tag;
+    }
 
-	public function delete() {
-		$imgURL = 'img/'.$this->id.'.png';
-		$thumbURL = 'img/'.$this->id.'_thumb.png';
-		if (is_file($imgURL)) {
-			unlink($imgURL);
-		}
-		if (is_file($thumbURL)) {
-			unlink($thumbURL);
-		}
-	}
+    private $id;
 
-	/**
-	 * Returns the right HTML tag to display an image.
-	 *
-	 * @param int $id
-	 * @return HTML tag
-	 */
-	public static function imgTag($id) {
-		$imgURL = 'img/'.$id.'.png';
-		if (!is_file($imgURL)) return '';
-		$thumbURL = 'img/'.$id.'_thumb.png';
-		if (is_file($thumbURL)) {
-			$thumbSize = getimagesize($thumbURL);
-			$tag = '<img src="'.$thumbURL.'" '.$thumbSize[3].' class="bookImage" />';
-			$tag = '<a href="'.$imgURL.'" target="_blank" title="Bild in Originalgröße">'.$tag.'</a>';
-		}
-		else {
-			$tag = '<img src="'.$imgURL.'" class="bookImage" />';
-		}
-		return $tag;
-	}
+    /**
+     * Constructor saving book id and determining image path.
+     *
+     * @param int $id The book-ID.
+     */
+    public function Image($id) {
+        $this->id = (int) $id;
+    }
 
-	/**
-	 * Checks all conditions for uploading images.
-	 * @return boolean
-	 */
-	public static function uploadable() {
-		if (defined('GD_VERSION')) {
-			return is_writable('img/');
-		}
-		else {
-			return false;
-		}
-	}
+    /**
+     * Moves reads the uploaded file and writes it in the image directory.
+     *
+     * @return boolean True on success.
+     */
+    public function moveUploaded() {
+        if (count($_FILES) != 1) return false;
+        if (!isset($_FILES['image'])) return false;
+        $tmp_name = $_FILES['image']['tmp_name'];
+        if (!is_uploaded_file($tmp_name)) return false;
+        $imageSize = getimagesize($tmp_name);
+        switch ($imageSize[2]) {
+            case IMAGETYPE_GIF:
+                $image = imagecreatefromgif($tmp_name);
+                break;
+            case IMAGETYPE_JPEG:
+                $image = imagecreatefromjpeg($tmp_name);
+                break;
+            case IMAGETYPE_PNG:
+                $image = imagecreatefrompng($tmp_name);
+                break;
+            default:
+                return false;
+        }
+        $this->delete();
+        imagepng($image, self::PATH . $this->id . '.png');
+        $max_thumb_width = 250;
+        $max_thumb_height = 250;
+        $img_width = imagesx($image);
+        $img_height = imagesy($image);
+        if ($img_width > $max_thumb_width || $img_height > $max_thumb_height) {
+            if ($img_width > $img_height) {
+                $thumb_width = $max_thumb_width;
+                $thumb_height = $thumb_width * $img_height / $img_width;
+            }
+            else {
+                $thumb_height = $max_thumb_height;
+                $thumb_width = $thumb_height * $img_width / $img_height;
+            }
+            $thumb = imagecreatetruecolor($thumb_width, $thumb_height);
+            imagecopyresized($thumb, $image, 0, 0, 0, 0, $thumb_width, $thumb_height, $img_width, $img_height);
+            imagepng($thumb, self::PATH . $this->id . '_thumb.png');
+            imagedestroy($thumb);
+        }
+        imagedestroy($image);
+        return true;
+    }
+
+    /**
+     * Removes all images belonging to a book.
+     * @return void
+     */
+    public function delete() {
+        $imgURL = self::PATH . $this->id . '.png';
+        $thumbURL = self::PATH . $this->id . '_thumb.png';
+        if (is_file($imgURL)) {
+            unlink($imgURL);
+        }
+        if (is_file($thumbURL)) {
+            unlink($thumbURL);
+        }
+    }
 
 }
 ?>
