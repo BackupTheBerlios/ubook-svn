4da1a5ad97cbe560e3311ffca0ddf3cb95b17785
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2010-04-27 15:54:49 +0000

    xhtml language tag

--- trunk/header.php
+++ trunk/header.php
@@ -21,7 +21,7 @@ function echo_navigation() {
 header('Content-Type: text/html;charset=utf-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml">
  <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <meta http-equiv="Content-Style-Type" content="text/css" />
