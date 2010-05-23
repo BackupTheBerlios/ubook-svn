bf20eeb2b817a120cd69eaea26e04669d4ca9324
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2010-05-23 19:12:33 +0000

    Template doc

--- trunk/tools/Template.php
+++ trunk/tools/Template.php
@@ -1,10 +1,11 @@
 <?php
 /*
  * This file is part of uBook - a website to buy and sell books.
+ * It's licensed under the GNU General Public License.
  * Copyright (C) 2010 Maikel Linke
 */
 
-/*
+/**
  * Substitutes special template tags with given content.
  *
  * This is just another template class for HTML/XML templates. BUT it is the
@@ -14,26 +15,33 @@
  * 1. How To Use - some examples
  * 2. Why This Way - good reasons for the syntax and the API
  *
- * == How To Use ==
+ * You can download this class as archive with documentation:
+ * - {@link http://ubook.berlios.de/template/Template.zip}
+ * Or you can directly browse in the source code at BerliOS:
+ * - {@link http://svn.berlios.de/svnroot/repos/ubook/branches/template/}
+ * - {@link http://svn.berlios.de/viewvc/ubook/branches/template/}
+ *
+ * Feedback is very welcome: ubook-info@lists.berlios.de
+ *
+ * <b>How To Use</b>
  *
  * Begin to create pure HTML. It could be like this:
- *
  * <pre>
- *     <p>Hello, this is normal text. But it is also possible to insert
- *        variable text here. For example the time: 'time'.</p>
- *     <p>Okay, this is easy. It's just a string replacement. But now follows
+ *     &lt;p&gt;Hello, this is normal text. But it is also possible to insert
+ *        variable text here. For example the time: 'time'.&lt;/p&gt;
+ *     &lt;p&gt;Okay, this is easy. It's just a string replacement. But now follows
  *        a subtemplate. It allows you to repeat some code over and over again,
- *        everytime with different content. For example a list of names:</p>
- *     <ul>
- *         <!-- BEGIN item -->
- *         <li>'name'</li>
- *         <!-- END item -->
- *     </ul>
+ *        everytime with different content. For example a list of names:&lt;/p&gt;
+ *     &lt;ul&gt;
+ *         &lt;!-- BEGIN item --&gt;
+ *         &lt;li&gt;'name'&lt;/li&gt;
+ *         &lt;!-- END item --&gt;
+ *     &lt;/ul&gt;
  * </pre>
  *
  * Now we have the template. Let's write code to fill it with data.
  *
- * <?php
+ * <code>
  *     $template = new Template($theHtmlCodeAsString);
  *     // or
  *     $template = Template::fromFile($filenameOfTemplate);
@@ -51,63 +59,76 @@
  *
  *     // ready...
  *     echo $template->result();
- * ?>
+ * </code>
  *
- * == Why This Way ==
+ * <b>Why This Way</b>
  *
  * This class was written for a simple project. It's just one class, not a whole
  * framework with caching and so on. It supports everything, that a simple
  * template engine should support. But not more.
  * Keep it simple.
  *
- * Why do you use single quotes for variables?
- * - There are three reasons:
- * 1. It's easy to write on most keyboards.
- * 2. Valid HTML will stay valid, when you insert single quotes in the text.
+ * <i>Why do you use single quotes for variables?</i>
+ *
+ * There are three reasons:
+ * <ol>
+ * <li> It's easy to write on most keyboards.</li>
+ * <li> Valid HTML will stay valid, when you insert single quotes in the text.
  * It's also no problem to insert single quotes into a double quoted parameter
- * value.
- * 3. You can prevent unintended replacements by encoding the user data via
- * htmlentities() before inserting into the template (see example below).
+ * value.</li>
+ * <li> You can prevent unintended replacements by encoding the user data via
+ * htmlentities() before inserting into the template (see example below).</li>
+ * </ol>
  *
  * Besides, if you like another syntax, just change the constants in the script.
  * It's free software.
  *
- * Why don't you recommend the assignArray-method?
- * - It's no good idea to store everything in an array. The more complex your
+ * <i>Why don't you recommend the assignArray-method?</i>
+ *
+ * It's no good idea to store everything in an array. The more complex your
  * application becomes, the harder it is to remember all the array structures.
  * Additionally some IDEs (e.g. NetBeans) support the correct renaming of
  * variables, but not of array indices, of course.
+ *
  * Assigning one variable to the template is not more complicated than a new
  * entry in an array.
- * And most IMPORTANT:
+ *
+ * And most <i>important</i>:
  * Perhaps you have a full array of data from mysql_fetch_array(). But you
  * should not insert this into a template. Normally you have more data in there
  * than you want to present the user. And you have to encode the user data to
  * prevent code injection. So depending on your application, perhaps you can do
  * something like this:
- * <?php
+ * <code>
  *     $arr = mysql_fetch_array($result);
  *     foreach ($arr as $key => $value) {
  *         $encoded = htmlentities($value, ENT_QUOTES, 'UTF-8');
  *         $template->assign($key, $encoded);
  *     }
- * ?>
+ * </code>
+ *
  * But this depends on your application.
  *
- * Wouldn't it be nice to define conditions for subtemplates or provide
- * something like a for-loop syntax?
- * - No. It's a good idea to seperate PHP and HTML cleanly. So you have all your
+ * <i>Wouldn't it be nice to define conditions for subtemplates or provide
+ * something like a for-loop syntax?</i>
+ *
+ * No. It's a good idea to seperate PHP and HTML cleanly. So you have all your
  * application logic in your PHP code. Your template files only define, what can
  * be displayed and how. If you really want to script within your HTML, then you
  * can just use PHP itself:
- * (german) http://php-coding-standard.de/php_template_engine.php
+ * - {@link http://articles.sitepoint.com/article/beyond-template-engine}
+ * - {@link http://php-coding-standard.de/php_template_engine.php} (german)
  *
- * What is the other way?
- * - Other interesting template classes:
- * * http://articles.sitepoint.com/article/beyond-template-engine
- * * http://template.ecoware.de/
- * * http://www.phpbar.de/w/P.E.T.
- * * http://kuerbis.org/asap/article/12/
+ * <i>What is the other way?</i>
+ *
+ * Other interesting template classes:
+ * - {@link http://articles.sitepoint.com/article/beyond-template-engine}
+ * - {@link http://template.ecoware.de/en/}
+ * - {@link http://www.phpbar.de/w/P.E.T.} (german)
+ * - {@link http://kuerbis.org/asap/article/12/}
+ *
+ * @author Maikel Linke (ubook-info@lists.berlios.de)
+ * @version 2010-05-23
 */
 class Template {
 
@@ -123,9 +144,9 @@ class Template {
      * Regular expression for subtemplates.
      *
      * It uses the following pattern modifiers:
-     * i - caseless
-     * m - multiline
-     * s - dotall
+     * - i - caseless
+     * - m - multiline
+     * - s - dotall
      */
     const SUB_PATTERN = '/<!--\s*BEGIN\s+([a-z0-9_\-]+)\s*-->(.*?)<!--\s*END\s+\\1\s*-->/ims';
 
@@ -136,8 +157,11 @@ class Template {
     private $subTemplateStrings = array();
 
     /**
-     * Loads the content of a template.
-     * @param string $filename path to the template
+     * Loads the content of a template file.
+     * @param string $filename path to the template file
+     * @return Template instance with the content of the template file
+     * @throws Exception if $filename doesn't point to a file or reading fails
+     * (plus Exception from the constructor)
      */
     public static function fromFile($filename) {
         if (!is_file($filename)) {
@@ -151,8 +175,9 @@ class Template {
     }
 
     /**
-     * Creates a new instance.
-     * @param string $content
+     * Creates a new instance with given string as source (not filename).
+     * @param string $content containing the code of the template
+     * @throws Exception if there are ambiguous subtemplates
      */
     public function __construct($content) {
         $this->content = $content;
@@ -170,8 +195,8 @@ class Template {
 
     /**
      * Assigns all values of the given array to the keys of the array.
-     * Warning: This function is seldom usefull. Don't assign arrays from your
-     * database without encoding the data. See documentation above.
+     * <b>Warning:</b> This function is seldom usefull. Don't assign arrays from
+     * your database without encoding the data. See documentation above.
      * @param array $associativeArray of the form array( 'tag_name' => 'value' )
      */
     public function assignArray($associativeArray) {
@@ -182,7 +207,8 @@ class Template {
      * Subtemplates are defined once and will appear in the result zero or more
      * times. Varying content and also sub-subtemplates are possible.
      * @param string $name identifyer of the subtemplate
-     * @return self a full template instance to customize this subtemplate
+     * @return Template a full template instance to customize this subtemplate
+     * @throws Exception if their is no such subtemplate
      */
     public function addSubtemplate($name) {
         if (!isset($this->subTemplates[$name])) {
@@ -203,6 +229,10 @@ class Template {
         return $c;
     }
 
+    /**
+     * Searches for subtemplates and stores them.
+     * @throws Exception if there are ambiguous subtemplates
+     */
     private function initSubtemplates() {
         preg_match_all(self::SUB_PATTERN, $this->content, $matches);
         for ($i = 0; $i < sizeof($matches[0]); $i++) {
