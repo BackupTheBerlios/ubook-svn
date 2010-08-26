55e97df6450d41e28a6deac215ecd3a4e112f6ee
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2010-08-26 16:18:42 +0000

    openSearch plugin available

---
+++ trunk/openSearch.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of uBook - a website to buy and sell books.
+ * Copyright (C) 2010 Maikel Linke
+ */
+
+require_once 'tools/Template.php';
+require_once 'tools/WEBDIR.php';
+
+$tmpl = Template::fromFile('view/openSearch.xml');
+$tmpl->assign('siteurl', WEBDIR);
+
+header('Content-Type: application/opensearchdescription+xml;charset=utf-8');
+//header('Content-Type: text/xml;charset=utf-8');
+echo $tmpl->result();
+?>
