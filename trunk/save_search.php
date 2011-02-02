69f2b84a29a78fe2594898287d52d6eef4891059
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2011-02-02 18:54:05 +0000

    bugfix: searches are reactivated without typing the mailadress again

--- trunk/save_search.php
+++ trunk/save_search.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of uBook - a website to buy and sell books.
- * Copyright (C) 2009 Maikel Linke
+ * Copyright (C) 2011 Maikel Linke
  */
 
 require_once 'books/SearchKey.php';
@@ -10,32 +10,28 @@ require_once 'notification/Searches.php';
 $searchKey = new SearchKey();
 
 if (!$searchKey->isGiven()) {
-	header('Location: ./');
+    header('Location: ./');
 }
 
+$mail = null;
+
+if (isset($_GET['mail'])) {
+    $mail = $_GET['mail'];
+}
 if (isset($_GET['name'])) { // field for mail address
-	$searches = new Searches();
-	$searches->addSearch($searchKey->asText(), $_GET['name']);
-	header('Location: ./?search=' . urlencode($searchKey->asText()) . '&searchSaved=1');
-} else {
-
+    $mail = $_GET['name'];
 }
 
-include 'header.php';
+if ($mail) {
+    $searches = new Searches();
+    $searches->addSearch($searchKey->asText(), $mail);
+    header('Location: ./?search=' . urlencode($searchKey->asText()) . '&searchSaved=1');
+}
+
+$tmpl = Template::fromFile("view/save_search.html");
+$tmpl->assign('searchKey', $searchKey->asHtml());
+$tmpl->assign('urlSearchKey', urlencode($searchKey->asHtml()));
+
+$output = new Output();
+$output->send($tmpl->result());
 ?>
-
-<h2>Suche nach '<?php echo $searchKey->asHtml(); ?>'</h2>
-<div>
-<form action="save_search.php" method="get">
- <input type="hidden" name="search" value="<?php echo $searchKey->asHtml(); ?>" />
- <label>Deine E-Mailadresse:<br />
- <input type="text" name="name" value="" size="20" alt="E-Mailadresse" />
- </label>
- <input type="submit" value="Suche abonnieren" />
-</form>
-</div>
-<p>
-&larr; <a href="./?search=<?php echo urlencode($searchKey->asText()); ?>">Zurück zur Suche</a>
-</p>
-
-<?php include 'footer.php'; ?>
