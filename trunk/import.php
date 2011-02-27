9bc30878936df73ad0023bb7fc68400d5be66593
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2011-02-27 16:46:39 +0000

    safer import

--- trunk/import.php
+++ trunk/import.php
@@ -2,13 +2,14 @@
 
 /*
  * This file is part of uBook - a website to buy and sell books.
- * Copyright (C) 2010 Maikel Linke
+ * Copyright (C) 2011 Maikel Linke
  */
 
 require_once 'magic_quotes.php';
 require_once 'books/Book.php';
 require_once 'tools/Mailer.php';
 require_once 'tools/Output.php';
+require_once 'tools/Parser.php';
 require_once 'tools/Template.php';
 require_once 'tools/SelectableCategories.php';
 
@@ -19,13 +20,15 @@ function import_book($bookString, Template $tmpl) {
     $bookLines = split("\n", $bookString, sizeof($labels));
     for ($i = 0; $i < sizeof($labels); $i++) {
         list($label, $value) = split(':', $bookLines[$i], 2);
-        if (trim($label) != $labels[$i])
+        if (trim($label) != $labels[$i]) {
             $value = '';
-        $tmpl->assign($indices[$i], trim($value));
+        }
+        $value = Parser::text2html(stripslashes(trim($value)));
+        $tmpl->assign($indices[$i], $value);
     }
 }
 
-$usermail = Mailer::mailFromUser('mail');
+$usermail = Parser::text2html(stripslashes(Mailer::mailFromUser('mail')));
 if (isset($_POST['book_data'])) {
     $tmpl = Template::fromFile('view/add_form.html');
     import_book($_POST['book_data'], $tmpl);
