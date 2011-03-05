47f9d8f2d3783087c50ad65442e3bc83c61e6922
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2011-03-05 15:21:51 +0000

    todo

--- trunk/about.php
+++ trunk/about.php
@@ -6,7 +6,7 @@
 
 require_once 'tools/Output.php';
 require_once 'text/Template.php';
-
+// TODO: target attribute not allowed in xhtml strict (search in all docs)
 $tmpl = Template::fromFile('view/about.html');
 $output = new Output();
 $output->setExpires(43200);
