6f3e4937af59a313ae01a1437fd446f58bb241fe
maikel <maikel@ae5323b6-6143-0410-9c18-fb529f046211>
2011-10-07 19:05:52 +0000

    license update

--- branches/doctest/DocTest.php
+++ branches/doctest/DocTest.php
@@ -2,8 +2,20 @@
 
 /*
  * This file is part of uBook - a website to buy and sell books.
- * It's licensed under the GNU General Public License.
- * Copyright (C) 2010 Maikel Linke
+ * Copyright © 2010 Maikel Linke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -72,7 +84,20 @@
  *
  * This class is part of the {@link http://ubook.berlios.de/ uBook Project}.
  *
- * @author Maikel Linke (ubook-info@lists.berlios.de)
+ * @author Maikel Linke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. (ubook-info@lists.berlios.de)
  * @version 2010-09-11
  */
 class DocTest {
